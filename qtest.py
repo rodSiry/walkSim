4c387668ec620e16dff6be8962ccb48154041785
Rodrigue Siry <rodsiry@gmail.com>
2017-09-17 15:51:01 +0200

    Added bounds

--- qtest.py
+++ qtest.py
@@ -12,9 +12,6 @@ from IO import *
 class Net(nn.Module):
     def __init__(self, n_input, n_hidden, n_output):
         super(Net, self).__init__()
-        self.fc1=nn.LSTMCell(n_input, n_hidden)
-        self.fc2=nn.LSTMCell(n_hidden, n_hidden)
-        self.fc3=nn.LSTMCell(n_hidden, n_hidden)
         self.fc4=nn.Linear(n_hidden, n_hidden )
         self.fc5=nn.Linear(n_hidden, n_hidden )
         self.fc6=nn.Linear(n_hidden, n_output)
@@ -24,26 +21,26 @@ class Net(nn.Module):
         self.activ=nn.LeakyReLU()
         self.h0=Variable(torch.randn(1,n_hidden))
         self.c0=Variable(torch.randn(1,n_hidden))
-        self.hi1=Variable(torch.randn(1,n_hidden))
-        self.hi2=Variable(torch.randn(1,n_hidden))
-        self.hi3=Variable(torch.randn(1,n_hidden))
-        self.ci1=Variable(torch.randn(1,n_hidden))
-        self.ci2=Variable(torch.randn(1,n_hidden))
-        self.ci3=Variable(torch.randn(1,n_hidden))
+        self.hi=[]
+        self.ci=[]
+        self.fc=[]
+        self.n_lstm=10
+        for i in range(self.n_lstm):
+            self.hi.append(Variable(torch.randn(1,n_hidden)))
+            self.ci.append(Variable(torch.randn(1,n_hidden)))
+            if(i==0):
+                self.fc.append(nn.LSTMCell(n_input, n_hidden))
+            else:
+                self.fc.append(nn.LSTMCell(n_hidden, n_hidden))
+
     def reinit(self):
-        self.hi1=self.h0
-        self.hi2=self.h0
-        self.hi3=self.h0
-        self.ci1=self.c0
-        self.ci2=self.c0
-        self.ci3=self.c0
+        for i in range(self.n_lstm):
+            self.hi[i]=self.h0
+            self.ci[i]=self.c0
     def forward(self, x):
-        self.hi1, self.ci1 =self.fc1(x, (self.hi1, self.ci1))
-        x=self.activ(self.hi1)
-        self.hi2, self.ci2 =self.fc2(x, (self.hi2, self.ci2))
-        x=self.activ(self.hi2)
-        self.hi3, self.ci3 =self.fc3(x, (self.hi3, self.ci3))
-        x=self.activ(self.hi3)
+        for i in range(self.n_lstm):
+            self.hi[i], self.ci[i] =self.fc[i](x, (self.hi[i], self.ci[i]))
+            x=self.activ(self.hi[i])
         x=self.activ(self.fc4(x))
         x=self.activ(self.fc5(x))
         x=self.activ(self.fc6(x))
@@ -56,7 +53,7 @@ def finish_episode():
         R= r +0.99* R
         rewards.insert(0,R)
     rewards=torch.Tensor(rewards)
-    rewards= ( rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+    rewards= ( rewards - rewards.mean()) / rewards.std()
     for action, r in  zip(network.saved_actions, rewards):
         action.reinforce(r)
     optimizer.zero_grad()
@@ -76,8 +73,8 @@ def simCycle():
 n_servo=8
 n_input=2*n_servo+1
 n_output=2*n_servo
-torque_int=1
-torque_int_std=30
+torque_int=20
+torque_int_std=20
         
 io=IO()
 network=Net(n_input,128, n_output)
@@ -86,7 +83,7 @@ while True:
     #RESET!
     observation=np.array(io.getMsg())
     network.reinit()
-    for j in range(10000):
+    while(True):
         obs_input= Variable(torch.from_numpy(observation).unsqueeze(0).float())
         prob=network(obs_input)
         action=torch.normal(Variable(torque_int*prob.data[0,0:n_servo], requires_grad=True), Variable(torque_int_std*torch.abs(prob.data[0,n_servo:]), requires_grad=True))
--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -15,12 +15,15 @@ bool Simulation::CheckFall()
 	bodies[1]->getMotionState()->getWorldTransform(t);
     btMatrix3x3 trans= t.getBasis();
 	btVector3 v=(trans.getColumn(1)).normalize();
-	scal=v.dot(btVector3(0.f, 1.f, 0.f));
-/*  if(bodies[1]->getCenterOfMassPosition().getY()<0.2 ||scal<0.1|| count>1000  )
+	scal=v.dot(btVector3(0.f, 1.f, 0.f)); /*  
+                                              
+
 		return true;
 	else
 		return false;*/
-    if(count>1000)
+    btVector3 pP=p;
+	p=bodies[1]->getCenterOfMassPosition();
+    if(count>1000|| p.getY()<1)
         return true;
     else 
         return false;
@@ -44,15 +47,15 @@ Simulation::Simulation():p(btVector3(0.,0.,0.)), box(Pave())
 	dynamicsWorld->setGravity(btVector3(0, -10, 0));
 	float up=1.5f;
 	AddPlane();
-	AddParr(1., 0.5, 1., 0.5, btVector3(0.,3.75+up,0.));
-	AddParr(1., 0.5, 0.5, 0.5, btVector3(1.,3.25+up,0.));
-	AddParr(1., 0.5, 0.5, 0.5, btVector3(-1.,3.25+up,0.));
-	AddParr(1., 0.25, 1., 0.25, btVector3(1.,2.5+up,0.75));
-	AddParr(1., 0.25, 1., 0.25, btVector3(-1.,2.5+up,0.75));
+	AddParr(1., 0.25, 1., 0.25, btVector3(0.,3.75+up,0.));
+	AddParr(1., 0.25, 0.25, 0.25, btVector3(1.,3.25+up,0.));
+	AddParr(1., 0.25, 0.25, 0.25, btVector3(-1.,3.25+up,0.));
+	AddParr(1., 0.25, 0.5, 0.25, btVector3(1.,1.75+up,0.));
+	AddParr(1., 0.25, 0.5, 0.25, btVector3(-1.,1.75+up,0.));
 	AddParr(1., 0.25, 1., 0.25, btVector3(1.5,1.+up,0.75));
 	AddParr(1., 0.25, 1., 0.25, btVector3(-1.5,1.+up,0.75));
-	AddParr(1., 0.25, 1., 0.25, btVector3(1.25,-0.5+up,0.75));
-	AddParr(1., 0.25, 1., 0.25, btVector3(-1.25,-0.5+up,0.75));
+	AddParr(1., 0.25, 0.5, 0.25, btVector3(1.25,-0.5+up,0.75));
+	AddParr(1., 0.25, 0.5, 0.25, btVector3(-1.25,-0.5+up,0.75));
 	values.push_back(30.f);
 	values.push_back(-30.f);
 	values.push_back(-45.f);
@@ -62,11 +65,11 @@ Simulation::Simulation():p(btVector3(0.,0.,0.)), box(Pave())
 	values.push_back(-0.f);
 	values.push_back(-0.f);
 	btVector3 vel(0,0,0);
-	btHingeConstraint* c= new btHingeConstraint(*bodies[1],*bodies[2],btVector3(0.5,-0.5,0.),btVector3(-0.5,0.,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
+	btHingeConstraint* c= new btHingeConstraint(*bodies[1],*bodies[2],btVector3(0.5,-0.5,0.),btVector3(-0.25,0.,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
-	c= new btHingeConstraint(*bodies[2],*bodies[4],btVector3(0.,0.,0.5),btVector3(-0.,0.75,-0.25),btVector3(0.,0.,1.),btVector3(0.,0.,1.),false);
+	c= new btHingeConstraint(*bodies[2],*bodies[4],btVector3(0.,-0.5,0.),btVector3(-0.,0.5,-0.),btVector3(0.,0.,1.),btVector3(0.,0.,1.),false);
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
@@ -74,11 +77,11 @@ Simulation::Simulation():p(btVector3(0.,0.,0.)), box(Pave())
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
-	c= new btHingeConstraint(*bodies[1],*bodies[3],btVector3(-0.5,-0.5,0.),btVector3(0.5,0.,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
+	c= new btHingeConstraint(*bodies[1],*bodies[3],btVector3(-0.5,-0.5,0.),btVector3(0.25,0.,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
-	c= new btHingeConstraint(*bodies[3],*bodies[5],btVector3(0.,0.,0.5),btVector3(-0.,0.75,-0.25),btVector3(0.,0.,-1.),btVector3(0.,0.,-1.),false);
+	c= new btHingeConstraint(*bodies[3],*bodies[5],btVector3(0.,-0.5,0.),btVector3(-0.,0.5,-0.),btVector3(0.,0.,-1.),btVector3(0.,0.,-1.),false);
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
@@ -86,21 +89,21 @@ Simulation::Simulation():p(btVector3(0.,0.,0.)), box(Pave())
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
-	c= new btHingeConstraint(*bodies[6],*bodies[8],btVector3(-0.25,-0.75,0.),btVector3(0.25,0.75,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
+	c= new btHingeConstraint(*bodies[6],*bodies[8],btVector3(-0.25,-0.75,0.),btVector3(0.25,0.5,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
-	c= new btHingeConstraint(*bodies[7],*bodies[9],btVector3(0.25,-0.75,0.),btVector3(-0.25,0.75,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
+	c= new btHingeConstraint(*bodies[7],*bodies[9],btVector3(0.25,-0.75,0.),btVector3(-0.25,0.5,0.),btVector3(1.,0.,0.),btVector3(1.,0.,0.),false);
 	c->enableAngularMotor(true, 0., 20.);
 	dynamicsWorld->addConstraint(c, false);
 	cs.push_back(c);
     for(int i(0);i<cs.size();i++){
         prevState.push_back(0);
+        cs[i]->setLimit(-1.5, 1.5);
     }
 
-
-
-//	bodies[1]->setLinearVelocity(vel);
+   //     cs[1]->setLimit(-0.5, 0.5);
+    //    cs[4]->setLimit(-0.5, 0.5);
 }
 
 void Simulation::AddParr(float mass, float x, float y, float z, btVector3 p)
@@ -123,6 +126,7 @@ void Simulation::AddPlane()
 	btVector3 fallInertia(0, 0, 0);
 	shp->calculateLocalInertia(0, fallInertia);
 	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(0, fallMotionState, shp, fallInertia);
+    fallRigidBodyCI.m_friction=1.;
 	btRigidBody* rigidBody = new btRigidBody(fallRigidBodyCI);
 	AddBody(rigidBody);
 }	
@@ -136,7 +140,7 @@ void Simulation::Draw(mat4 projection, mat4 model, mat4 view)
 	p=bodies[1]->getCenterOfMassPosition();
 	double pScal=scal;
 	scal=v.dot(btVector3(0.f, 1.f, 0.f));
-	fit=p.getY()+scal;
+	fit=(p.getY()+scal);
 	dynamicsWorld->stepSimulation(1 / 60.f, 10);
 	for(int i(1);i<bodies.size();i++)
 		box.Draw(1,1,1,projection, getMat4(bodies[i]), view, dimensions[i-1]);
@@ -151,7 +155,6 @@ void Simulation::ComputeServos()
 {
 	for(int i(0);i<cs.size();i++)
 	{
-//		cs[i]->setLimit(b,b)
 		float targetVelocity = 0.f;
 		if(values[i]!=0.f)
 			targetVelocity = values[i]/abs(values[i])*9999.f;
@@ -203,15 +206,15 @@ std::vector<double> Simulation::GetState(){
 	{
 		res.push_back(90/M_PI*cs[i]->getHingeAngle());
 	}
-
 	for(int i(0);i<cs.size();i++)
     {
        res.push_back(90/M_PI*cs[i]->getHingeAngle()-prevState[i]);
        prevState[i]=90/M_PI*cs[i]->getHingeAngle();
     }
-    btTransform attitude;
-	bodies[1]->getMotionState()->getWorldTransform(attitude);
-	btVector3 v=(attitude*btVector3(0.f,1.f,0.f)).normalize();
+    btTransform t;
+	bodies[1]->getMotionState()->getWorldTransform(t);
+    btMatrix3x3 trans= t.getBasis();
+	btVector3 v=(trans.getColumn(1)).normalize();
     res.push_back(v.dot(btVector3(0.f,1.f,0.f)));
 	return res;
 			
